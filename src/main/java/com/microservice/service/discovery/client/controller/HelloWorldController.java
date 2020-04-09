e81150de0634bcef259c7cbde9ae35892802f227
andser <andra_s3rban@yahoo.com>
2020-04-09 23:08:32 +0300

    add connection to config-server

--- src/main/java/com/microservice/service/discovery/client/controller/HelloWorldController.java
+++ src/main/java/com/microservice/service/discovery/client/controller/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.microservice.service.discovery.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloWorldController {
 
+    @Value("${user.role}")
+    String userRole;
+
     @GetMapping(path = "/greeting")
     public String gretingEndpoint() {
-        return "Hello World!";
+        return "Hello World! + " + userRole;
     }
 
 }
